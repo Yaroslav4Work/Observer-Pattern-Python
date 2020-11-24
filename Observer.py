6ae8684e3f1b1880148479f4ae1eebf8fd2649f1
Yaroslav 4 Work <yaroslav4work@gmail.com>
2020-11-24 16:31:47 +0300

    Initial commit

---
+++ Observer.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+from random import randrange
+from typing import List
+import Subject
+
+
+class Observer(ABC):
+    """
+    Интерфейс Наблюдателя объявляет метод уведомления, который издатели
+    используют для оповещения своих подписчиков.
+    """
+
+    @abstractmethod
+    def update(self, subject: Subject) -> None:
+        """
+        Получить обновление от субъекта.
+        """
+        pass
---
+++ Subject.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+from abc import ABC, abstractmethod
+import Observer
+
+
+class Subject(ABC):
+    """
+    Интферфейс издателя объявляет набор методов для управлениями подписчиками.
+    """
+
+    @abstractmethod
+    def attach(self, observer: Observer) -> None:
+        """
+        Присоединяет наблюдателя к издателю.
+        """
+        pass
+
+    @abstractmethod
+    def detach(self, observer: Observer) -> None:
+        """
+        Отсоединяет наблюдателя от издателя.
+        """
+        pass
+
+    @abstractmethod
+    def notify(self) -> None:
+        """
+        Уведомляет всех наблюдателей о событии.
+        """
+        pass
